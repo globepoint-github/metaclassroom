6dd1cb01a621f3e0e60615f1bc9a76099bc0f108
seunginlee0711 <si.lee@globepoint.co.kr>
2024-08-06 09:26:06 +0900

    - 게시판 조회 / 댓글 입력 만드는 중

---
+++ back_dev/src/main/generated/com/java/metaclassroom/entity/board/QBoard.java
@@ -0,0 +1,74 @@
+package com.java.metaclassroom.entity.board;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = -523990062L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final StringPath answer = createString("answer");
+
+    public final EnumPath<com.java.metaclassroom.enums.AnswerType> answerType = createEnum("answerType", com.java.metaclassroom.enums.AnswerType.class);
+
+    public final QBoardConfig boardConfig;
+
+    public final StringPath classify = createString("classify");
+
+    public final QComment comment;
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final EnumPath<com.java.metaclassroom.enums.AnswerYN> isDelete = createEnum("isDelete", com.java.metaclassroom.enums.AnswerYN.class);
+
+    public final EnumPath<com.java.metaclassroom.enums.AnswerYN> isOpen = createEnum("isOpen", com.java.metaclassroom.enums.AnswerYN.class);
+
+    public final EnumPath<com.java.metaclassroom.enums.AnswerYN> isTopExpo = createEnum("isTopExpo", com.java.metaclassroom.enums.AnswerYN.class);
+
+    public final StringPath thumbnail = createString("thumbnail");
+
+    public final StringPath title = createString("title");
+
+    public final NumberPath<Integer> views = createNumber("views", Integer.class);
+
+    public QBoard(String variable) {
+        this(Board.class, forVariable(variable), INITS);
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QBoard(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QBoard(PathMetadata metadata, PathInits inits) {
+        this(Board.class, metadata, inits);
+    }
+
+    public QBoard(Class<? extends Board> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.boardConfig = inits.isInitialized("boardConfig") ? new QBoardConfig(forProperty("boardConfig")) : null;
+        this.comment = inits.isInitialized("comment") ? new QComment(forProperty("comment"), inits.get("comment")) : null;
+    }
+
+}
+
---
+++ back_dev/src/main/generated/com/java/metaclassroom/entity/board/QComment.java
@@ -0,0 +1,65 @@
+package com.java.metaclassroom.entity.board;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QComment is a Querydsl query type for Comment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QComment extends EntityPathBase<Comment> {
+
+    private static final long serialVersionUID = -144834485L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QComment comment = new QComment("comment");
+
+    public final com.java.metaclassroom.common.QBaseEntity _super = new com.java.metaclassroom.common.QBaseEntity(this);
+
+    public final StringPath answer = createString("answer");
+
+    public final QBoard board;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final EnumPath<com.java.metaclassroom.enums.AnswerYN> isDelete = createEnum("isDelete", com.java.metaclassroom.enums.AnswerYN.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedDate = _super.modifiedDate;
+
+    public final StringPath userName = createString("userName");
+
+    public QComment(String variable) {
+        this(Comment.class, forVariable(variable), INITS);
+    }
+
+    public QComment(Path<? extends Comment> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QComment(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QComment(PathMetadata metadata, PathInits inits) {
+        this(Comment.class, metadata, inits);
+    }
+
+    public QComment(Class<? extends Comment> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.board = inits.isInitialized("board") ? new QBoard(forProperty("board"), inits.get("board")) : null;
+    }
+
+}
+
