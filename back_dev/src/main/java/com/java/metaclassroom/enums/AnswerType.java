997e5ccfba2bc489681cde60b4bfc9d7a93b3904
seunginlee0711 <si.lee@globepoint.co.kr>
2024-08-14 18:32:02 +0900

    - 일반게시판 목록 수정 중

--- back_dev/src/main/java/com/java/metaclassroom/enums/AnswerType.java
+++
@@ -1,6 +0,0 @@
-package com.java.metaclassroom.enums;
-
-public enum AnswerType {
-   W , // 대기
-    C // 완료
-}
--- back_dev/src/main/java/com/java/metaclassroom/enums/BoardType.java
+++
@@ -1,7 +0,0 @@
-package com.java.metaclassroom.enums;
-
-public enum BoardType{
-    N, // notice 공지사항
-    Q, // qna
-    F // faq
-}
